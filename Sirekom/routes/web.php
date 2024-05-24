62e077690478fbc776894d3309bc420943c48cd9
AutoTampan <radinkarahadian@gmail.com>
2024-05-24 23:17:18 +0700

    revisi

--- Sirekom/routes/web.php
+++ Sirekom/routes/web.php
@@ -57,3 +57,8 @@ Route::get('/mahasiswa/profile/{$id}', [ProfileController::class, 'edit']);
 Route::put('/mahasiswa/profile/{$id}', [ProfileController::class, 'update']);
 
 Route::resource('lomba', LombaController::class);
+
+Route::get('/mahasiswa/detailInfodanSubmit', function () {
+    return view('app.mahasiswa.detailInfodanSubmit');
+});
+
